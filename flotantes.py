79f4b8c576d62dfe4ab4bc87c76804de041f8f72
daniel salamanca <sebassala1992@hotmail.es>
2020-08-08 10:47:28 -0500

    expliacion de algorimo de enumeracion para calcular raiz cuadrad

---
+++ flotantes.py
@@ -0,0 +1,13 @@
+def run():
+    x = 0.0
+    for i in range(10):
+        x += 0.1
+    
+    if x < 1.0 and x > 0.99999:
+        print(f'x ={x}')
+    else:
+        print(f'x != {x}')
+
+
+if __name__ == "__main__":
+    run()
